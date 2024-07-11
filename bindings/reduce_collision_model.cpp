4e70474760e1a61d0cd8227340642dd04cf754e6
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-07-11 14:15:20 +0000

    Export reduce_collision_model in python

---
+++ bindings/reduce_collision_model.cpp
@@ -0,0 +1,24 @@
+
+
+#include <pinocchio/multibody/fwd.hpp>
+#include <boost/python.hpp>
+#include <boost/python/enum.hpp>
+#include <eigenpy/eigenpy.hpp>
+#include <boost/python/return_internal_reference.hpp>
+#include <vector>
+
+#include "panda_torque_mpc/reduce_collision_model.h"
+#include "panda_torque_mpc/python.h"
+
+namespace panda_torque_mpc
+{
+namespace pin = pinocchio;
+namespace bp = boost::python;
+
+void exposeReduceCollisionModel()
+{
+  bp::register_ptr_to_python<boost::shared_ptr<pin::GeometryModel> >();
+  bp::def("reduce_capsules_robot", &reduce_capsules_robot);
+}
+
+}  // namespace panda_torque_mpc
